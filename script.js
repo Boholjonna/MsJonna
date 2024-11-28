9cf3db90010a8a72539ba3448306695caf48f66a
Jonna Bohol BSCPE 1-G DAY PROGRAM <159886481+Boholjonna@users.noreply.github.com>
2024-11-28 21:01:43 +0800

    revised, created new design

--- script.js
+++
@@ -1,8 +0,0 @@
-// Select the button element
-const button = document.querySelector('.click');
-
-// Add event listener to button click
-button.addEventListener('click', () => {
-  // Hide all elements in the body by setting their display to 'none'
-  document.querySelectorAll('body > *').forEach(el => el.style.display = 'none');
-});
